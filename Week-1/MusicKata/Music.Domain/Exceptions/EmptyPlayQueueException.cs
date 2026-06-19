4e7fb79662de5ce918a441c2f12f573e9c907479
Bryan Uriel Ramirez Espinosa <bryan.ramirez9289@alumnos.udg.mx>
2026-06-19 16:25:18 -0600

    [feat]: exceptions have sealed type

--- Week-1/MusicKata/Music.Domain/Exceptions/EmptyPlayQueueException.cs
+++ Week-1/MusicKata/Music.Domain/Exceptions/EmptyPlayQueueException.cs
@@ -1,6 +1,6 @@
 namespace MusicKata.Domain;
 
-public class EmptyPlayQueueException : MusicStoreException
+public sealed class EmptyPlayQueueException : MusicStoreException
 {
     public EmptyPlayQueueException()
         : base("The mixtape queue is empty — add tracks before playing.") { }
